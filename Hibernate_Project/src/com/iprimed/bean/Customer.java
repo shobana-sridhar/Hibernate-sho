c820375669fb4e78395d76f5f85c33604f3e6124
shobana-sridhar <57611363+shobana-sridhar@users.noreply.github.com>
2019-12-27 12:24:56 +0530

    hibernate

---
+++ Hibernate_Project/src/com/iprimed/bean/Customer.java
@@ -0,0 +1,54 @@
+package com.iprimed.bean;
+
+public class Customer {
+	
+	//Properties to represent Customer
+	private int id;
+	private String name;
+	private String email;
+	private long mobile;
+	
+	//Getters and Setters method used to retrieve the values 
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	
+	//no argument constructor
+	Customer(){}
+	
+	//parameterized constructor
+	public Customer(String name,String email,long mobile)
+	{
+		this.name=name;
+		this.email=email;
+		this.mobile=mobile;
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Hibernate_Project/src/com/iprimed/dao/AddCustomer.java
@@ -0,0 +1,29 @@
+package com.iprimed.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.iprimed.bean.Customer;
+
+public class AddCustomer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class).buildSessionFactory();
+		
+		Session session=factory.getCurrentSession();
+		
+		try {
+			//create object
+			
+			
+		}
+		finally {
+			//sessions are closed
+			session.close();
+			factory.close();
+		}
+	}
+
+}
